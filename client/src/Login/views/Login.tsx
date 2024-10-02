bfb03ec78c1839184ebc0416da0522cb49878716
Gaurav <gauravkumarkarki07@gmail.com>
2024-10-02 14:28:07 +1000

    Forms and validation

--- client/src/Login/views/Login.tsx
+++ client/src/Login/views/Login.tsx
@@ -1,7 +1,46 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
+
+export interface LoginData {
+  usernameOrEmail: string;
+  password: string;
+}
 
 function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginData>();
+
+  const formSubmit = (validData: LoginData) => {
+    console.log(validData)
+  }
   return (
-    <div>login</div>
+    <section className="flex flex-col gap-4 max-w-[800px] mx-auto mt-20">
+      <h1 className="text-xl font-semibold">Welcome to the site ! Please Login</h1>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit(formSubmit)}>
+        <section className="flex flex-col gap-2 w-full h-[80px]">
+          <Label>Username Or Email</Label>
+          <Input
+            {...register('usernameOrEmail', { required: 'Username or Email is required' })}
+          />
+          {errors.usernameOrEmail && <span className="text-xs text-red-500">{errors.usernameOrEmail.message}</span>}
+        </section>
+        <section className="flex flex-col gap-2 w-full h-[80px]">
+          <Label>Password</Label>
+          <Input
+            type="password"
+            {...register('password', { required: 'Password is required' })}
+          />
+          {errors.password && <span className="text-xs text-red-500">{errors.password.message}</span>}
+        </section>
+        <Button variant={'primary'}>Login</Button>
+      </form>
+      <section className="flex flex-row gap-2 text-sm">
+        <span>Don't have an account ?</span>
+        <Link className="text-blue-500 hover:underline" to={'/register'}>Register</Link>
+      </section>
+    </section>
   )
 }
 
--- client/src/Register/views/Register.tsx
+++ client/src/Register/views/Register.tsx
@@ -1,7 +1,93 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom"
 
+
+export interface RegisterData {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 function Register() {
+  const { register, handleSubmit, formState: { errors },watch } = useForm<RegisterData>();
+
+  // Watch Password
+  const watchPassword=watch("password");
+
+  const formSubmit = (validData: RegisterData) => {
+    console.log(validData);
+  }
   return (
-    <div>register</div>
+    <section className="flex flex-col gap-4 max-w-[800px] mx-auto mt-20">
+      <h1 className="text-xl font-semibold">Register Your Account</h1>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit(formSubmit)}>
+        {/* First name, and last name */}
+        <section className="flex flex-row gap-4">
+          <section className="flex flex-col gap-2 w-full h-[80px]">
+            <Label>First Name</Label>
+            <Input
+              {...register('firstName', { required: 'First Name is required' })}
+            />
+            {errors.firstName && <span className="text-xs text-red-500">{errors.firstName.message}</span>}
+          </section>
+          <section className="flex flex-col gap-2 w-full h-[80px]">
+            <Label>Last Name</Label>
+            <Input
+              {...register('lastName', { required: 'Last Name is required' })}
+            />
+            {errors.lastName && <span className="text-xs text-red-500">{errors.lastName.message}</span>}
+          </section>
+        </section>
+        <section className="flex flex-col gap-2 w-full h-[80px]">
+          <Label>Username</Label>
+          <Input
+            {...register('username', { required: 'Username is required' })}
+          />
+          {errors.username && <span className="text-xs text-red-500">{errors.username.message}</span>}
+        </section>
+        <section className="flex flex-col gap-2 w-full h-[80px]">
+          <Label>Email</Label>
+          <Input
+            type="email"
+            {...register('email', { required: 'Email is required' })}
+          />
+          {errors.email && <span className="text-xs text-red-500">{errors.email.message}</span>}
+        </section>
+        <section className="flex flex-row gap-4">
+
+          <section className="flex flex-col gap-2 w-full h-[80px]">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              {...register('password', { required: 'Password is required' })}
+            />
+            {errors.password && <span className="text-xs text-red-500">{errors.password.message}</span>}
+          </section>
+          <section className="flex flex-col gap-2 w-full h-[80px]">
+            <Label>Confirm Password</Label>
+            <Input
+              type="password"
+              {...register('confirmPassword', {
+                required: 'Confirm Password is required',
+                validate: value => value === watchPassword || 'Passwords do not match'
+              })}
+            />
+            {errors.confirmPassword && <span className="text-xs text-red-500">{errors.confirmPassword.message}</span>}
+          </section>
+        </section>
+
+        <Button variant={'primary'}>Register</Button>
+      </form>
+      <section className="flex flex-row gap-2 text-sm">
+        <span>Have an account ?</span>
+        <Link className="text-blue-500 hover:underline" to={'/login'}>Login</Link>
+      </section>
+    </section>
   )
 }
 
